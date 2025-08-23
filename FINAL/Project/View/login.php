7e5cb2fc4dccb4b155071349ae3573fb07a05d1f
Ashraful-IR <rifo0118@gmail.com>
2025-08-23 14:03:00 +0600

    c

--- FINAL/Project/View/login.php
+++ FINAL/Project/View/login.php
@@ -1,22 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log In</title>
 </head>
+
 <body>
     <h1>Log In</h1>
-    <form action="login.php" method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
-        <br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
-        <br>
-        <button type="submit">Log In</button>
-    </form>
-    <p>Don't have an account? <a href="register.php">Register here</a>.</p>
-    
+    <div class="login-container">
+        <table>
+            <form action="login.php" method="post">
+                <tr>
+                    <td>
+                        <label for="username">Username:</label>
+                        <input type="text" id="username" name="username" required>
+                    </td>
+                </tr>
+                <br>
+                <tr>
+                    <td>
+                        <label for="password">Password:</label>
+                        <input type="password" id="password" name="password" required>
+                        <br>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <button type="submit">Log In</button>
+                    </td>
+                </tr>
+            </form>
+            <tr>
+                <td>
+                    <p>Don't have an account? <a href="register.php">Register here</a>.</p>
+                </td>
+            </tr>
+        </table>
+    </div>
 </body>
+
 </html>
