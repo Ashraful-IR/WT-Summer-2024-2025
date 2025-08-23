89ac3e16edda29932b827153ccf69e4050af574a
Ashraful-IR <rifo0118@gmail.com>
2025-08-23 16:58:57 +0600

    login htmml css 50% done by Ashraful Islam

--- FINAL/Log In/View/login.php
+++ FINAL/Log In/View/login.php
@@ -5,46 +5,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log In</title>
-    <link rel="stylesheet" href="../CSS/loginstyle.css">
+    <link rel="stylesheet" href="../CSS/loginstyle.css" type="text/css">
 </head>
 
 <body>
-    
-    <div class="login-container">
-        <table>
-            <form action="login.php" method="post">
-                <tr>
-                    <td>
-                        <label for="username">Username:</label>
-                    </td>
-                    <td>
-                        <input type="text" id="username" name="username" required>
-                    </td>
-                <tr>
-                    <td>
-                        <label for="password">Password:</label>
-                    </td>
-                    <td>
-                        <input type="password" id="password" name="password" required>
-                    </td>
-                </tr>
 
-
-                <tr>
-                    <td>
-                        <button type="submit">Log In</button>
-                    </td>
-                </tr>
-                </td>
-            </form>
-            <tr>
-                <td>
-                    <p>Don't have an account? <a href="register.php">Register here</a>.</p>
-                </td>
-            </tr>
-            </td>
-        </table>
+    <div class="login-box">
+        <div class="input-box">
+            <span class="icon">
+                <ion-icon name="mail-unread"></ion-icon>
+            </span>
+            <input type="email" class="" required>
+            <label for="">Email</label>
+        </div>
+        <div class="input-box">
+            <span class="icon">
+                <ion-icon name="lock-closed"></ion-icon>
+            </span>
+            <input type="password" class="" required>
+            <label for="">Password</label>
+        </div>
+        <div class="remember-forgate">
+            <label><input type="checkbox">Remeber me</label>
+            <a href="">Forgot Password?</a>
+        </div>
+        <div >
+            <button type="submit" class="log-btn">Log In</button>
+        </div>
+        <div class="register">
+            <p>Dont have an account? <a href="">Create an account</a></p>
+        </div>
     </div>
+
+
+
+
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>
