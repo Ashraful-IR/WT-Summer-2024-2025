8c934e7c1063745f9a1a3c650a40d47b9ffea97a
Ashraful-IR <rifo0118@gmail.com>
2025-08-23 22:34:57 +0600

    s

--- FINAL/Log In/View/login.php
+++ FINAL/Log In/View/login.php
@@ -15,25 +15,27 @@
             <span class="icon">
                 <ion-icon name="mail-unread"></ion-icon>
             </span>
-            <input type="email" class="" required>
+            <input type="email" class="text" required>
             <label for="">Email</label>
         </div>
         <div class="input-box">
             <span class="icon">
                 <ion-icon name="lock-closed"></ion-icon>
             </span>
-            <input type="password" class="" required>
+            <input type="password" class="text" required>
             <label for="">Password</label>
         </div>
         <div class="remember-forgate">
             <label><input type="checkbox">Remeber me</label>
             <a href="">Forgot Password?</a>
         </div>
-        <div >
+        <div>
             <button type="submit" class="log-btn">Log In</button>
         </div>
         <div class="register">
-            <p>Dont have an account? <a href="">Create an account</a></p>
+            <p>Dont have an account?
+                <a href="#">Create an account</a>
+            </p>
         </div>
     </div>
 
