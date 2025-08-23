dc90e4f7ce039405ce48deea38eb7254aac72384
Ashraful-IR <rifo0118@gmail.com>
2025-08-23 14:23:44 +0600

    z

--- FINAL/Log In/View/login.php
+++ FINAL/Log In/View/login.php
@@ -5,38 +5,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log In</title>
+    <link rel="stylesheet" href="../CSS/loginstyle.css">
 </head>
 
 <body>
-    <h1>Log In</h1>
+    
     <div class="login-container">
         <table>
             <form action="login.php" method="post">
                 <tr>
                     <td>
                         <label for="username">Username:</label>
+                    </td>
+                    <td>
                         <input type="text" id="username" name="username" required>
                     </td>
-                </tr>
-                <br>
                 <tr>
                     <td>
                         <label for="password">Password:</label>
+                    </td>
+                    <td>
                         <input type="password" id="password" name="password" required>
-                        <br>
                     </td>
                 </tr>
+
+
                 <tr>
                     <td>
                         <button type="submit">Log In</button>
                     </td>
                 </tr>
+                </td>
             </form>
             <tr>
                 <td>
                     <p>Don't have an account? <a href="register.php">Register here</a>.</p>
                 </td>
             </tr>
+            </td>
         </table>
     </div>
 </body>
