e0eccb3849bd5378370923282890600e844e4b7e
Ashraful-IR <rifo0118@gmail.com>
2025-08-28 20:59:15 +0600

    SSDDSDSD

--- FINAL/Project/View/signup.php
+++ FINAL/Project/View/signup.php
@@ -56,7 +56,10 @@
                 </td>
             </tr>
             </div>
-          
+          <td>
+            <label for="lname">Phone</label><br>
+            <input type="text" name="phone"id="phone"class="password"/>
+          </td>
         </tr>
         <tr>
           <td>
